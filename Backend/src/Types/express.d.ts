c6f6dc3a91f29fda4d43d5414168a50a69e992a4
Precious <pemakenemi56@gmail.com>
2025-03-17 15:43:23 -0700

    minor changes in dockerfile for railway

---
+++ Backend/src/Types/express.d.ts
@@ -0,0 +1,13 @@
+// src/types/express.d.ts
+import { Express } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        [key: string]: any;
+      };
+    }
+  }
+}
