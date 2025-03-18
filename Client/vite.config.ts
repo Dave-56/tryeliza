5276ad93d322556f05251b27a72d68a90201eaf9
Precious <pemakenemi56@gmail.com>
2025-03-17 21:45:23 -0700

    trying to fix vercel build

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current directory.
