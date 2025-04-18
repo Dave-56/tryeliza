824404dd14763d0d2b2534aa1565777c6496e174
Precious Emakenemi <pemakenemi56@gmail.com>
2025-04-18 13:43:53 -0700

    Update emailUtils.ts
    reduce no of emails to 20 for inital sync to reduce sync time

--- Backend/src/services/Google/emailUtils.ts
+++ Backend/src/services/Google/emailUtils.ts
@@ -224,7 +224,7 @@ export class EmailUtils extends GoogleClient {
             const response = await this.gmail.users.messages.list({
                 userId: 'me',
                 q: `in:inbox after:${formattedDate}`,  // Gmail search query format: YYYY/MM/DD
-                maxResults: 40 // Limit to 20 messages for now - we can comment this out later
+                maxResults: 20 // Limit to 20 messages for now - we can comment this out later
             });
 
             console.log("Initial messages response:", {
